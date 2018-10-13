58a0c1739cf0ccee5e6a61f6dfdd055d2c9aa7d9
louis <lucn@bsoft.com.cn>
2018-10-13 13:03:31 +0800

    加入spring定时任务

---
+++ src/main/java/com/louis/controller/FlightTrainTask.java
@@ -0,0 +1,24 @@
+package com.louis.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+/**
+ * @Author: lcn_louis
+ * @Date: 2018/10/11 下午1:49
+ */
+
+@EnableScheduling
+@Component
+public class FlightTrainTask {
+    private static final Logger log = LoggerFactory.getLogger(FlightTrainTask.class);
+
+    @Scheduled(cron = "*/5 * * * * ?") // 间隔5秒执行
+    public void taskCycle() {
+        log.info("使用SpringMVC框架配置定时任务");
+        System.out.println("使用SpringMVC框架配置定时任务");
+    }
+}
