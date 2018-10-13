3920ed302c750792410f6d4be557c045bd1424a6
louis <lucn@bsoft.com.cn>
2018-10-13 13:26:08 +0800

    加入spring定时任务

--- src/main/java/com/louis/controller/FlightTrainTask.java
+++ src/main/java/com/louis/controller/FlightTrainTask.java
@@ -2,7 +2,6 @@ package com.louis.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -11,7 +10,6 @@ import org.springframework.stereotype.Component;
  * @Date: 2018/10/11 下午1:49
  */
 
-@EnableScheduling
 @Component
 public class FlightTrainTask {
     private static final Logger log = LoggerFactory.getLogger(FlightTrainTask.class);
